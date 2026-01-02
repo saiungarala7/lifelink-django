8c363ac6507695c14bb0ea77dd98f092f452f895
saiungarala7 <venkatungarala2005@gmail.com>
2026-01-02 23:40:05 +0530

    Enable DEBUG for media support

--- lifelink/settings.py
+++ lifelink/settings.py
@@ -17,7 +17,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 SECRET_KEY = 'django-insecure-dev-key-change-in-production-123456789'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
