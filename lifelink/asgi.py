71a04f357a0c5de9352d7e48bfb19988eba45148
saiungarala7 <venkatungarala2005@gmail.com>
2026-01-03 09:21:11 +0530

    Fix ASGI startup and media routing

--- lifelink/asgi.py
+++ lifelink/asgi.py
@@ -1,28 +1,17 @@
-"""
-ASGI config for lifelink project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lifelink.settings')
 
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            __import__("chat.routing").routing.websocket_urlpatterns
         )
     ),
 })
--- lifelink/urls.py
+++ lifelink/urls.py
@@ -10,6 +10,23 @@ from django.views.generic import RedirectView
 from django.conf.urls.static import static
 
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('accounts.urls')),
+#     path('donor/', include('donors.urls')),
+#     path('bloodbank/', include('bloodbanks.urls')),
+#     path('patient/', include('patients.urls')),
+#     path('chat/', include('chat.urls')),
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(
+#     settings.MEDIA_URL,
+#     document_root=settings.MEDIA_ROOT
+# )
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
@@ -21,9 +38,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
 
